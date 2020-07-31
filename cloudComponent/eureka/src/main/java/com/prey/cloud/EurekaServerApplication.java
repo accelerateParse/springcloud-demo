2b5a12be5d457170e43c08a9d26592ee249390c7
accelerlerate <1353814960@qq.com>
2020-07-31 10:40:29 +0800

    eureka注册中心

---
+++ cloudComponent/eureka/src/main/java/com/prey/cloud/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.prey.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author prey
+ * @description: Eureka 服务注册中心
+ **/
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
